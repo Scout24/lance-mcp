9ba2d4e39d3a13226fc9c9cce80f0fdf01e5b945
Oliver Schmitz-Hennemann <oliver.schmitz@scout24.com>
2025-03-07 10:30:50 +0100

    add confluence pages (WIP)

--- src/seed.ts
+++ src/seed.ts
@@ -19,14 +19,18 @@ import { PromptTemplate } from "@langchain/core/prompts";
 import * as crypto from 'crypto';
 import * as defaults from './config'
 import { TextLoader } from "langchain/document_loaders/fs/text";
+import { ConfluencePagesLoader } from '@langchain/community/document_loaders/web/confluence'
+import readline from 'readline'
 
 const argv: minimist.ParsedArgs = minimist(process.argv.slice(2),{boolean: "overwrite"});
 
 const databaseDir = argv["dbpath"];
 const filesDir = argv["filesdir"];
 const overwrite = argv["overwrite"];
+const confluenceBaseUrl = "https://gs24.atlassian.net/wiki";
 
-function validateArgs() {
+
+    function validateArgs() {
     if (!databaseDir || !filesDir) {
         console.error("Please provide a database path (--dbpath) and a directory with files (--filesdir) to process");
         process.exit(1);
@@ -36,6 +40,14 @@ function validateArgs() {
     console.log("FILES DIRECTORY: ", filesDir);
     console.log("OVERWRITE FLAG: ", overwrite);
 }
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
 
 const contentOverviewPromptTemplate = `Write a high-level one sentence content overview based on the text below:
 
@@ -75,116 +87,167 @@ const directoryLoader = new DirectoryLoader(
   },
 );
 
-const model = new Ollama({ model: defaults.SUMMARIZATION_MODEL });
+rl.question('Please enter your Confluence API token: ', (confluenceApiToken) => {
+    rl.close();
 
-// prepares documents for summarization
-// returns already existing sources and new catalog records
-async function processDocuments(rawDocs: any, catalogTable: lancedb.Table, skipExistsCheck: boolean) {
-    // group rawDocs by source for further processing
-    const docsBySource = rawDocs.reduce((acc: Record<string, any[]>, doc: any) => {
-        const source = doc.metadata.source;
-        if (!acc[source]) {
-            acc[source] = [];
+
+    const confluencePagesLoader = new ConfluencePagesLoader(
+        {
+            baseUrl: confluenceBaseUrl,
+            spaceKey: "DOCI",
+            username: "oliver.schmitz@scout24.com",
+            accessToken: confluenceApiToken,
+        },
+    );
+
+
+    const model = new Ollama({model: defaults.SUMMARIZATION_MODEL});
+
+    // prepares documents for summarization
+    // returns already existing sources and new catalog records
+    async function processDocuments(rawDocs: Document[], catalogTable: lancedb.Table, skipExistsCheck: boolean) {
+        // group rawDocs by source for further processing
+        const docsBySource = rawDocs.reduce((acc: Record<string, Document[]>, doc: Document) => {
+            const source = doc.metadata.source;
+            if (!acc[source]) {
+                acc[source] = [];
+            }
+            acc[source].push(doc);
+            return acc;
+        }, {});
+
+        let skipSources = [];
+        let catalogRecords = [];
+
+        // iterate over individual sources and get their summaries
+        for (const [source, docs] of Object.entries(docsBySource)) {
+            console.log(source)
+            console.log(docs.length)
+
+            var hash:string
+            if ( source.startsWith( confluenceBaseUrl )) {
+                if (docs[0].metadata.version !== null && docs[0].metadata.version !== undefined) {
+                    hash = crypto.createHash('sha256').update(`${source}-${docs[0].metadata.version}`).digest('hex');
+                }else{
+                    throw new Error("Version is required for confluence pages")
+                }
+            } else {
+                // Calculate hash of the source document
+                const fileContent = await fs.promises.readFile(source);
+                hash = crypto.createHash('sha256').update(fileContent).digest('hex');
+            }
+            // Check if a source document with the same hash already exists in the catalog
+            const exists = skipExistsCheck ? false : await catalogRecordExists(catalogTable, hash);
+            if (exists) {
+                console.log(`Document with hash ${hash} already exists in the catalog. Skipping...`);
+                skipSources.push(source);
+            } else {
+                const contentOverview = await generateContentOverview(docs, model);
+                console.log(`Content overview for source ${source}:`, contentOverview);
+                catalogRecords.push(new Document({pageContent: contentOverview["text"], metadata: {source, hash}}));
+            }
         }
-        acc[source].push(doc);
-        return acc;
-    }, {});
 
-    let skipSources = [];
-    let catalogRecords = [];
-
-    // iterate over individual sources and get their summaries
-    for (const [source, docs] of Object.entries(docsBySource)) {
-        // Calculate hash of the source document
-        const fileContent = await fs.promises.readFile(source);
-        const hash = crypto.createHash('sha256').update(fileContent).digest('hex');
-
-        // Check if a source document with the same hash already exists in the catalog
-        const exists = skipExistsCheck ? false : await catalogRecordExists(catalogTable, hash);
-        if (exists) {
-            console.log(`Document with hash ${hash} already exists in the catalog. Skipping...`);
-            skipSources.push(source);
-        } else {
-            const contentOverview = await generateContentOverview(docs, model);
-            console.log(`Content overview for source ${source}:`, contentOverview);
-            catalogRecords.push(new Document({ pageContent: contentOverview["text"], metadata: { source, hash } }));
-        }
+        return {skipSources, catalogRecords};
     }
 
-    return { skipSources, catalogRecords };
-}   
+    async function seed() {
+        validateArgs();
 
-async function seed() {
-    validateArgs();
+        const db = await lancedb.connect(databaseDir);
 
-    const db = await lancedb.connect(databaseDir);
+        let catalogTable: lancedb.Table;
+        let catalogTableExists = true;
+        let chunksTable: lancedb.Table;
 
-    let catalogTable : lancedb.Table;
-    let catalogTableExists = true;
-    let chunksTable : lancedb.Table;
-
-    try {
-        catalogTable = await db.openTable(defaults.CATALOG_TABLE_NAME);
-    } catch (e) {
-        console.error(`Looks like the catalog table "${defaults.CATALOG_TABLE_NAME}" doesn't exist. We'll create it later.`);
-        catalogTableExists = false;
-    }
-
-    try {
-        chunksTable = await db.openTable(defaults.CHUNKS_TABLE_NAME);
-    } catch (e) {
-        console.error(`Looks like the chunks table "${defaults.CHUNKS_TABLE_NAME}" doesn't exist. We'll create it later.`);
-    }
-
-    // try dropping the tables if we need to overwrite
-    if (overwrite) {
         try {
-            await db.dropTable(defaults.CATALOG_TABLE_NAME);
-            await db.dropTable(defaults.CHUNKS_TABLE_NAME);
+            catalogTable = await db.openTable(defaults.CATALOG_TABLE_NAME);
         } catch (e) {
-            console.log("Error dropping tables. Maybe they don't exist!");
+            console.error(`Looks like the catalog table "${defaults.CATALOG_TABLE_NAME}" doesn't exist. We'll create it later.`);
+            catalogTableExists = false;
         }
+
+        try {
+            chunksTable = await db.openTable(defaults.CHUNKS_TABLE_NAME);
+        } catch (e) {
+            console.error(`Looks like the chunks table "${defaults.CHUNKS_TABLE_NAME}" doesn't exist. We'll create it later.`);
+        }
+
+        // try dropping the tables if we need to overwrite
+        if (overwrite) {
+            try {
+                await db.dropTable(defaults.CATALOG_TABLE_NAME);
+                await db.dropTable(defaults.CHUNKS_TABLE_NAME);
+            } catch (e) {
+                console.log("Error dropping tables. Maybe they don't exist!");
+            }
+        }
+
+        // load files from the files path
+        console.log("Loading files...")
+        const fileDocs = await directoryLoader.load();
+        //const fileDocs = []
+        console.log(`found ${fileDocs.length} files`);
+        const confluenceDocs = await confluencePagesLoader.load()
+        //const confluenceDocs = []
+        console.log(`found ${confluenceDocs.length} confluence pages`);
+
+        const rawDocs : Document[] = [...fileDocs, ...confluenceDocs];
+        // overwrite the metadata as large metadata can give lancedb problems
+        for (const doc of rawDocs) {
+            doc.metadata = {loc: doc.metadata.loc, source: doc.metadata.source ??= doc.metadata.url , version: doc.metadata.version};
+        }
+
+        console.log("Loading LanceDB catalog store...")
+
+        const {
+            skipSources,
+            catalogRecords
+        } = await processDocuments(rawDocs, catalogTable, overwrite || !catalogTableExists);
+        const catalogStore = catalogRecords.length > 0 ?
+            await LanceDB.fromDocuments(catalogRecords,
+                new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}),
+                {
+                    mode: overwrite ? "overwrite" : undefined,
+                    uri: databaseDir,
+                    tableName: defaults.CATALOG_TABLE_NAME
+                } as LanceDBArgs) :
+            new LanceDB(new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}), {
+                uri: databaseDir,
+                table: catalogTable
+            });
+        console.log(catalogStore);
+
+        console.log("Number of new catalog records: ", catalogRecords.length);
+        console.log("Number of skipped sources: ", skipSources.length);
+        //remove skipped sources from rawDocs
+        const filteredRawDocs = rawDocs.filter((doc: any) => !skipSources.includes(doc.metadata.source));
+
+        console.log("splitting documents...")
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 500,
+            chunkOverlap: 10,
+        });
+        const docs = await splitter.splitDocuments(filteredRawDocs);
+
+        console.log("updating / creating LanceDB vector store...")
+
+        const vectorStore = docs.length > 0 ?
+            await LanceDB.fromDocuments(docs,
+                new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}),
+                {
+                    mode: overwrite ? "overwrite" : undefined,
+                    uri: databaseDir,
+                    tableName: defaults.CHUNKS_TABLE_NAME
+                } as LanceDBArgs) :
+            new LanceDB(new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}), {
+                uri: databaseDir,
+                table: chunksTable
+            });
+
+        console.log("Number of new chunks: ", docs.length);
+        console.log(vectorStore);
     }
 
-    // load files from the files path
-    console.log("Loading files...")
-    const rawDocs = await directoryLoader.load();
-
-    // overwrite the metadata as large metadata can give lancedb problems
-    for (const doc of rawDocs) {
-        doc.metadata = { loc: doc.metadata.loc, source: doc.metadata.source };
-    }
-
-    console.log("Loading LanceDB catalog store...")
-
-    const { skipSources, catalogRecords } = await processDocuments(rawDocs, catalogTable, overwrite || !catalogTableExists);
-    const catalogStore = catalogRecords.length > 0 ? 
-        await LanceDB.fromDocuments(catalogRecords, 
-            new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}), 
-            { mode: overwrite ? "overwrite" : undefined, uri: databaseDir, tableName: defaults.CATALOG_TABLE_NAME } as LanceDBArgs) :
-        new LanceDB(new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}), { uri: databaseDir, table: catalogTable});
-    console.log(catalogStore);
-
-    console.log("Number of new catalog records: ", catalogRecords.length);
-    console.log("Number of skipped sources: ", skipSources.length);
-    //remove skipped sources from rawDocs
-    const filteredRawDocs = rawDocs.filter((doc: any) => !skipSources.includes(doc.metadata.source));
-
-    console.log("Loading LanceDB vector store...")
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 500,
-        chunkOverlap: 10,
-      });
-    const docs = await splitter.splitDocuments(filteredRawDocs);
-    
-    const vectorStore = docs.length > 0 ? 
-        await LanceDB.fromDocuments(docs, 
-        new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}), 
-        { mode: overwrite ? "overwrite" : undefined, uri: databaseDir, tableName: defaults.CHUNKS_TABLE_NAME } as LanceDBArgs) :
-        new LanceDB(new OllamaEmbeddings({model: defaults.EMBEDDING_MODEL}), { uri: databaseDir, table: chunksTable });
-
-    console.log("Number of new chunks: ", docs.length);
-    console.log(vectorStore);
-}
-
-seed();
+    seed();
+});
